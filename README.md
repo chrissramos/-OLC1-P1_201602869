54e44bbbfbc830e4fac9a40cd0eaf2c71d5678d6
Randall Ramos <randall.saucedo@gmail.com>
2020-06-04 02:59:40 -0600

    hola

---
+++ README.md
@@ -0,0 +1,7 @@
+## Practica 1
+### Compi1
+
+developed by **Randall Ramos**<br>
+id: 201602869<br>
+email: randall.saucedo@gmail.com<br>
+
---
+++ src/olc1/p1_201602869/OLC1P1_201602869.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olc1.p1_201602869;
+
+import grafico.VentanaPrincipal;
+
+/**
+ *
+ * @author Chriss Ramos
+ */
+public class OLC1P1_201602869 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaPrincipal p = new VentanaPrincipal();
+        p.setVisible(true);
+    }
+    
+}
